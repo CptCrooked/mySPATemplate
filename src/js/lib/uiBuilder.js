5d9bc42e35fd5edddee2f555adc1379d0403aa6e
André Botha <crookdev@outlook.com>
2026-02-07 12:55:02 +0200

    Commit before cloning project to other machine

--- src/js/lib/uiBuilder.js
+++ src/js/lib/uiBuilder.js
@@ -20,3 +20,4 @@ export const wrapperEl = (
     wrapperEl.classList.add(className);
   });
 };
+
--- src/main.js
+++ src/main.js
@@ -1,19 +1,42 @@
+import {
+  authForDevPurposes,
+  router,
+  ROUTES,
+} from "../../supStack/testApp/src/js/router/router";
+import { wrapperEl } from "./js/lib/uiBuilder";
 import "./style.scss";
-import { addToHistory } from "./js/components/router.js";
-
-addToHistory;
 
 const app = document.getElementById("app")
   ? document.getElementById("app")
   : null;
 
-console.log(app);
-
-window.addEventListener("pushstate", function (event) {
+window.addEventListener("pushstate", (e) => {
+  const state = e.arguments[0];
+  const route = e.arguments[2];
   console.log("A custom pushstate event was fired!");
   // You can access the arguments passed to pushState via event.arguments if you added them
-  console.log("State object:", event.arguments[0]);
+  console.log("State object:", state);
   // console.log('URL:', event.arguments[2]);
 
   // Your custom logic to update UI, perform tracking, etc. goes here
+
+  const page = wrapperEl("section", [{ "data-route": route }]);
+
+  switch (route) {
+    case ROUTES.LOGIN_OR_SIGNUP:
+      console.log(`Home`);
+
+      break;
+    case ROUTES.DASH:
+      console.log(`Dash`);
+
+      break;
+    case ROUTES.ARTICLES:
+      break;
+
+    default:
+      break;
+  }
 });
+
+router("/articles", {}, "", authForDevPurposes);
